560510a35462d00a76b87b960180be889c74d0ff
Jorge Lopez <jorgext@gmail.com>
2020-02-28 16:04:50 -0300

    updated ROOT macro

--- utils/makeFigure2.C
+++ utils/makeFigure2.C
@@ -20,9 +20,9 @@ void makeFigure2() {
     SetAtlasStyle(43,22);
     gStyle->SetEndErrorSize(0);
 
-    const char *variantname = "BLE30";
+    const char *variantname = "BL30";
 
-    TFile *fin = TFile::Open(Form("OutputROOT.2020227.%s.root",variantname)); // 20181229 nominal
+    TFile *fin = TFile::Open(Form("OutputROOT.20200228.%s.root",variantname)); // 20181229 nominal
     // TFile *fin_uncertainties = TFile::Open("./OutputROOT_ToyMC_ModelUncertianties.root");
     std::pair<TGraphErrors*,TGraphErrors*> tg[4];
     std::pair<TGraphErrors*,TGraphErrors*> tg_up[4];
