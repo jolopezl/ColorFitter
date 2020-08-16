62d1b05563c26449001f5d4616363e6cc5458f2b
Jorge Lopez <jorgext@gmail.com>
2020-08-15 22:37:48 -0400

    removed unnecesary folder

--- utils/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lopez/cernbox/phd/ColorFitter/analysis-HERMES/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp
-# Build directory: /Users/lopez/cernbox/phd/ColorFitter/analysis-HERMES/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("util")
--- utils/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lopez/cernbox/phd/ColorFitter/analysis-HERMES/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp/util
-# Build directory: /Users/lopez/cernbox/phd/ColorFitter/analysis-HERMES/pythia/esteban/LHAPDF-6.0.5/src/yamlcpp/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
